5556896c54538adb91035e4ec60c367368837e94
IvanaNest <ivananestorovic00@gmail.com>
2024-04-25 00:38:44 +0200

    Initial commit

---
+++ staticfiles/myapp/js/script.js
@@ -0,0 +1,18 @@
+function openTab(evt, tabName) {
+    console.log("openTab function called with tabName:", tabName);
+    var i, tabContent, tabLinks;
+    tabContent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+    tabLinks = document.getElementsByClassName("tab");
+    for (i = 0; i < tabLinks.length; i++) {
+        tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Set the first tab as active by default
+document.getElementById("matching").style.display = "block";
+document.querySelector(".tab").className += " active";
